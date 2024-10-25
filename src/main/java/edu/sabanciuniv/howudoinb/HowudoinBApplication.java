52b0ce8569a8784bf0599b48e489d2717387aa49
Nuh Al-Sharafi <103935821+N-alsharafi@users.noreply.github.com>
2024-10-25 12:00:33 +0300

    project created

---
+++ src/main/java/edu/sabanciuniv/howudoinb/HowudoinBApplication.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.howudoinb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowudoinBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowudoinBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sabanciuniv/howudoinb/HowudoinBApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.howudoinb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowudoinBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
