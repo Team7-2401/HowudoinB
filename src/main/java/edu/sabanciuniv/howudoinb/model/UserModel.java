580d82977860ec8c84db953a9aa16d41bf7c773e
Ali Ishtay <alijpfu0@gmail.com>
2024-11-11 17:46:21 +0300

    Modified UserModel Friend's list and removed username feild.

--- src/main/java/edu/sabanciuniv/howudoinb/model/UserModel.java
+++ src/main/java/edu/sabanciuniv/howudoinb/model/UserModel.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Data
@@ -11,8 +12,8 @@ import java.util.List;
 public class UserModel{
     private String email;
     private String password;
-    private String username;
+    private String lastname;
     private String name;
-    private List<UserModel>friends;
+    private List<Map<UserModel,String>>friends;
     private String aboutme;
 }
