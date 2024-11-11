58e5615710fcf3815d7b71b52d0311f734d97890
Ali Ishtay <alijpfu0@gmail.com>
2024-11-11 23:10:47 +0300

    Modified UserModel and added constarints for regestration validation.

--- src/main/java/edu/sabanciuniv/howudoinb/model/UserModel.java
+++ src/main/java/edu/sabanciuniv/howudoinb/model/UserModel.java
@@ -5,15 +5,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 import java.util.Map;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class UserModel{
-    private String email;
-    private String password;
-    private String lastname;
+
+    @NotBlank(message = "Name is required.")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters.")
     private String name;
+
+    @NotBlank(message = "Last name is required.")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters.")
+    private String lastName;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Email must be a valid format.")
+    private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
+    private String password;
+
     private List<Map<UserModel,String>>friends;
+
     private String aboutme;
 }
